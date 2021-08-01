45323a880a9d7374af4201120b1e2c02c7ee7f3f
craig-clemens <anthony.c.clemens@gmail.com>
2021-08-01 14:35:46 -0400

    total & lot summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mecha_car_mpg))
 # r-squared: 0.6825
 # p-value: 5.35e-11
+
+#Deliverable 2
+
+#Import
+suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names = FALSE, stringsAsFactors = FALSE)
+#Summary Dataframe
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+#Lot Summary Dataframe
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
